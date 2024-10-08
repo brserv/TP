08f477bc697a55b178392eb9f48536ee051ad77e
root <brieuc74000@gmail.com>
2024-10-08 14:23:58 +0200

    Test led clignotante

--- main.c
+++ main.c
@@ -1,16 +1,33 @@
 #include "main.h"
 
-uint32* const GPIOA_ODR = 0x40020014;
-uint32* const RCC_AHB1ENR = 0x40023830;
-uint32* const GPIOA_MODER = 0x48000000;
+uint* const GPIOA_ODR = 0x40020014;
+uint* const RCC_AHB1ENR = 0x40023830;
+uint* const GPIOA_MODER = 0x48000000;
+
+
 int main(void)
 {
-	RCC->AHB1ENR |= (1<<5); 		// Enable Clock GPIOA 	(RCC->AHB1ENR)
-	GPIOA->MODER |= (1<<10);// PA5 Output			(GPIOA->MODER)
+	*RCC_AHB1ENR |= (1<<5); 		// Enable Clock GPIOA 	(RCC->AHB1ENR)
+	*GPIOA_MODER |= (1<<10);// PA5 Output			(GPIOA->MODER)
 
 	while(1){
-		for(uint32_t i = 0 ; i<100000; i++);
-		GPIOA_ODR ^= (1<<5);	// Toggle PA5		(GPIOA->ODR)	
-		delay(1000);
+		for(uint32_t i = 0 ; i<100000; i++){
+		*GPIOA_ODR ^= (1<<5);	// Toggle PA5		(GPIOA->ODR)	
+		//delay(1000);
+		}
 	}
 }
+
+
+/*void delay(uint time)
+{
+	uint start = time();
+	uint time_now = 0;
+	
+
+	while(time_now < time)
+	{
+		time_now = time();
+		time_now = start - time_now; 
+	}
+}*/
--- main.h
+++ main.h
@@ -1,3 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <time.h>
+
+//void delay(uint time);
